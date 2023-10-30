62ad86294807b0f47b2d53c6b9338f5605c9f414
dvijtest <dvijtest@gmail.com>
2023-10-30 15:58:29 +0530

    adding hide post

--- plugin.rb
+++ plugin.rb
@@ -2,9 +2,3 @@
 # about: add a button at the end of every post allowing users to hide the post
 # version: 0.1
 # authors: cap_dvij
-
-enabled_site_setting :plugin_name_enabled
-
-module ::MyPluginModule
-  PLUGIN_NAME = "tcc hide post"
-end
